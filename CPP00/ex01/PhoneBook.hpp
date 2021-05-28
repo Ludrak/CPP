108aeda7087af6faec425fcf23c99c853d817ab2
Ludrak <luca06.robino@gmail.com>
2021-05-28 02:10:18 +0200

    deleted useless edit_contact() function

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -19,7 +19,6 @@ class   PhoneBook
         PhoneBook(void);
 
         uint8_t add_contact(const Contact new_contact);
-        uint8_t edit_contact(const std::string old_contact, const Contact new_contact);
         Contact *get_contacts(void) const;
         uint8_t get_nb_contact(void) const;
 };
